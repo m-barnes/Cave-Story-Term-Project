18210e8db5ab662b45437da8f446c41ba6d407be
Michael Barnes <m.brandon.barnes@gmail.com>
2020-03-25 12:24:23 -0500

    Week 8 Level Class Part 3

---
+++ Cave Story Week 8 Level Class_part 3/rectangle.h
@@ -0,0 +1,90 @@
+#pragma once
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include "globals.h"
+
+class Rectangle {
+public:
+	Rectangle() {}
+
+	Rectangle(int x, int y, int width, int height) :
+		_x(x),
+		_y(y),
+		_width(width),
+		_height(height)
+	{}
+
+
+	const int getCenterX()  const {
+		return this->_x + this->_width / 2;
+	}
+
+	const int getCenterY() const {
+		return this->_y + this->_height / 2;
+	}
+
+	const int getLeft() const {
+		return this->_x;
+	}
+
+	const int getRight() const {
+		return this->_x + this->_width;
+	}
+
+	const int getTop() const {
+		return this->_y;
+	}
+
+	const int getBottom() const {
+		return this->_y + this->_height;
+	}
+
+	const int getWidth() {
+		return this->_width;
+	}
+
+	const int getHeight() {
+		return this->_height;
+	}
+
+	const int getSide(const sides::Side side) const {
+		return
+			side == sides::Side::LEFT ? this->getLeft() :
+			side == sides::Side::RIGHT ? this->getRight() :
+			side == sides::Side::TOP ? this->getTop() :
+			side == sides::Side::BOTTOM ? this->getBottom() :
+			side == sides::Side::NONE;
+	}
+
+	//collision function
+	const bool collidesWith(const Rectangle& other) const {
+		return
+			this->getRight() >= other.getLeft() &&
+			this->getLeft() <= other.getRight() &&
+			this->getTop() <= other.getBottom() &&
+			this->getBottom() >= other.getTop();
+	}
+
+
+	//is the rectangle a valid rectangle?
+	const bool isValidRectangle() const {
+		return(this->_x >= 0 && this->_y >= 0 && this->_width >= 0 && this->_height >= 0);
+	}
+private:
+
+	int _x;
+	int _y;
+	int _width;
+	int _height;
+	
+
+
+};
+
+
+
+
+
+
+#endif
---
+++ Cave Story Week 8 Level Class_part 3/sprite.h
@@ -0,0 +1,44 @@
+#pragma once
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include "graphics.h"
+#include "globals.h"
+#include "rectangle.h"
+
+#include <SDL.h>
+#include <string>
+
+using namespace std;
+
+class Sprite {
+public:
+	//default constructor NOT USED
+	Sprite();
+	//custom constructor. Grabs the spritesheet asset and parses out a specific rectangle (source x and y, width, height, posX, posY)
+	Sprite(Graphics& graphics, const string& filePath, int sourceX, int sourceY, int width, int height, float posX, float posY);
+
+	//a way to get rid of all derived sprite objects 
+	virtual ~Sprite();
+
+	//a way to update and clear out specific sprite objects
+	virtual void update(); 
+
+	//draw the sprite to a specific location
+	void draw(Graphics& graphics, int x, int y); 
+
+	const Rectangle getBoundingBox() const;
+	const sides::Side getCollisionSide(Rectangle& other) const;
+
+protected:
+	SDL_Rect _sourceRect;
+	SDL_Texture* _spriteSheet;
+	Rectangle _boundingBox;
+	float _x, _y;
+
+private:
+	
+
+};
+
+#endif
