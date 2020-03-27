31bb81db1119b208dd8103f59704b2e5d1bfd2d9
Michael Barnes <m.brandon.barnes@gmail.com>
2020-03-27 13:17:45 -0500

    Week 10 Animated Tiles

---
+++ Cave Story Week 10 Animated Tiles/tile.h
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef TILE_H
+#define TILE_H
+
+#include "globals.h"
+#include "graphics.h"
+#include <SDL.h>
+
+class Tile {
+public:
+	//default constructor
+	Tile();
+
+	//custom constructor
+	Tile(SDL_Texture* tileset, Vector2 size, Vector2 tilesetPosition, Vector2 position);
+
+	//update function for the tile class
+	void update(int elapsedTime);
+
+	//draw function for the tile class
+	void draw(Graphics& graphics);
+
+
+protected:
+
+	SDL_Texture* _tileset;
+	Vector2 _size;
+	Vector2 _tilesetPosition;
+	Vector2 _position;
+
+};
+
+#endif
